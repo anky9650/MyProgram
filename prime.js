2fa71ca9b8876396d1d7db24f4db0909e1a46e73
anky9650 <ankush.zapak@gmail.com>
2022-12-09 00:44:06 +0530

    primeDone

---
+++ prime.js
@@ -0,0 +1,24 @@
+  let flag=0;
+
+
+let num=13; 
+for(i=2;i<num;i++){
+       
+ if(num%i==0){
+       
+ flag=flag+1
+       
+ }
+      
+ }
+ 
+ if(flag==0){
+ console.log("Yes"); 
+ 
+ }
+ 
+ else{
+  
+ console.log("No");
+  
+ } 
